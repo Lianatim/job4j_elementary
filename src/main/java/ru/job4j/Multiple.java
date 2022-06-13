844fe730c45b8cdabc227a6ef5229f179113037a
Lianatim <95958723+Lianatim@users.noreply.github.com>
2022-06-13 18:10:36 +0300

    Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -4,7 +4,7 @@ public class Multiple {
     public static void main(String[] args) {
         for (int i=0; i<=10; i++) {
             int number = 1;
-            System.out.println(number + "*" + i +" = " +number*i);
+            System.out.println(number + " * " + i +" = " +number*i);
         }
     }
 }
