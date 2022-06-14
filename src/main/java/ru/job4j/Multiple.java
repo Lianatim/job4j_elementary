61e1efecebb7e44e318a598432dfcc414724a263
Lianatim <95958723+Lianatim@users.noreply.github.com>
2022-06-14 20:41:11 +0300

    Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,9 +2,9 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        for (int i=0; i<=10; i++) {
+        for (int i = 0; i <= 10; i++) {
             int number = 1;
-            System.out.println(number + " * " + i +" = " +number*i);
+            System.out.println(number + " * " + i + " = " + number * i);
         }
     }
 }
