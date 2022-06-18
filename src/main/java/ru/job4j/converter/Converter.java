fda965681ed7d3f79a62402871f3e6d9f1a521b4
Lianatim <95958723+Lianatim@users.noreply.github.com>
2022-06-18 20:59:54 +0300

    Расстояние между точками в системе координат [#188 #313631]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -12,18 +12,12 @@ public class Converter {
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(160);
+        float dollar = Converter.rubleToDollar(180);
         System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("160 rubles are " + dollar + " dollar.");
-        float in = 140;
-        float expected = 2;
-        float out = Converter.rubleToEuro(in);
-        boolean passed = expected == out;
+        System.out.println("180 rubles are " + dollar + " dollar.");
+        boolean passed = 2 == euro;
         System.out.println("140 rubles are 2. Test result : " + passed);
-        float in2 = 160;
-        float expected2 = 2.6666667f;
-        float out2 = Converter.rubleToDollar(in2);
-        boolean passed2 = expected2 == out2;
-        System.out.println("160 rubles are 2.6666667. Test result : " + passed2);
+        boolean passed2 = 3 == dollar;
+        System.out.println("180 rubles are 3. Test result : " + passed2);
     }
 }
