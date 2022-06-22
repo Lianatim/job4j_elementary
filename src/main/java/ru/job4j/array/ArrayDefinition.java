0d699dd5a67106a55f5efa665442fec9b9000669
Lianatim <95958723+Lianatim@users.noreply.github.com>
2022-06-22 20:17:34 +0500

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Геральт";
+        names[1] = "Арагорн";
+        names[2] = "Гермиона";
+        names[3] = "Алиса";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
