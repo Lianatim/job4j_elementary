cdcef1fbfae09b9c5e7146d47fc1c29fff7cc3cd
Lianatim <95958723+Lianatim@users.noreply.github.com>
2022-06-23 10:32:12 +0500

    Ипотека [#156315 #315316]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        int duty = 0;
-        while (duty >= 0) {
-            duty = (int) (amount + amount * percent / 100 - salary);
-            amount = duty;
+        while (amount > 0) {
+            amount += amount + amount * percent / 100 - salary;
             year++;
         }
         return year;
